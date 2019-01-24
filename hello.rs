181ca26630cf881b5d2b9de111c05c09816ffca8
Farkas Péter <farkas.peter1995@gmail.com>
2019-01-25 00:00:57 +0100

    Added Rust and SQL

---
+++ hello.rs
@@ -0,0 +1,6 @@
+// Hello World in Rust
+
+fn main() {
+    println!("Hello, world!");
+    println!("Helló, világ!");
+}
---
+++ hello.sql
@@ -0,0 +1,4 @@
+-- Hello World in SQL
+
+SELECT 'Hello, world!' AS english, 
+       'Helló, világ!' AS magyar;
